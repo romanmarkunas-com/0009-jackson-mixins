d7176a620a784553ac65c47f1afd71897baff806
Romans Markuns <romans.markuns@transficc.com>
2020-06-03 22:34:49 +0100

    add failing example with immutable pojo

--- src/main/java/com/romanmarkunas/blog/jackson/ImmutableZoo.java
+++ src/main/java/com/romanmarkunas/blog/jackson/ImmutableZoo.java
@@ -1,12 +1,12 @@
 package com.romanmarkunas.blog.jackson;
 
-public class ZooFollowingJavaPropertiesConvention {
+public class ImmutableZoo {
 
     private final int giraffeCount;
     private final boolean open;
 
 
-    public ZooFollowingJavaPropertiesConvention(int giraffeCount, boolean open) {
+    public ImmutableZoo(int giraffeCount, boolean open) {
         this.giraffeCount = giraffeCount;
         this.open = open;
     }
--- src/test/java/com/romanmarkunas/blog/jackson/ZooTest.java
+++ src/test/java/com/romanmarkunas/blog/jackson/ZooTest.java
@@ -2,20 +2,58 @@ package com.romanmarkunas.blog.jackson;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ZooTest {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Test
+    void canDeserializeIfPojoIsJacksonFriendly() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonIn = "{\"giraffeCount\":5,\"open\":true}";
+
+        JacksonFriendlyZoo zoo = objectMapper.readValue(jsonIn, JacksonFriendlyZoo.class);
+        String jsonOut = objectMapper.writeValueAsString(zoo);
+
+        assertEquals(jsonIn, jsonOut);
+    }
 
 
     @Test
-    void canEasilyDeserializeIfPojoIsJacksonFriendly() throws JsonProcessingException {
-        String jsonIn = "{\"giraffeCount\":5,\"open\":true}";
-        JacksonFriendlyZoo zoo = objectMapper.readValue(jsonIn, JacksonFriendlyZoo.class);
+    void canDeserializeWithoutDefaultConstructor() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String expectedOut = "{\"giraffeCount\":42,\"open\":false}";
+
+        ImmutableZoo zoo = new ImmutableZoo(42, false);
         String jsonOut = objectMapper.writeValueAsString(zoo);
-        assertEquals(jsonIn, jsonOut);
+
+        assertEquals(expectedOut, jsonOut);
+    }
+
+    @Test
+    void failsToDeserializeWithoutDefaultConstructor() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonIn = "{\"giraffeCount\":5,\"open\":true}";
+
+        assertThrows(
+                InvalidDefinitionException.class,
+                () -> objectMapper.readValue(jsonIn, ImmutableZoo.class)
+        );
+    }
+
+    @Test
+    @Disabled
+    void canDeserializeWithoutDefaultConstructorButWithMixIn() {
+        ObjectMapper objectMapper = new ObjectMapper().addMixIn(ImmutableZoo.class, null);
+        String jsonIn = "{\"giraffeCount\":5,\"open\":true}";
+
+        assertThrows(
+                InvalidDefinitionException.class,
+                () -> objectMapper.readValue(jsonIn, ImmutableZoo.class)
+        );
     }
 }
